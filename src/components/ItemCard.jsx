bd5deb5dd0e45777f404a8aa4b89ed62955c934e
OkunoD <108437247+OkunoD@users.noreply.github.com>
2023-06-06 16:50:28 -0700

    added outfit container and itemCard

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { addJacketActionCreator, deleteJacketActionCreator, tryOnJacketActionCreator } from '../actions/actions.js'
+import './styles.css'; 
+
+let key = undefined;
+
+const mapStateToProps = function(state, ownProps) {
+  return {
+    itemId: state.wornTop.id,
+    itemName: state.wornTop.name,
+    itemColor: state.wornTop.color,
+    imgUrl: state.wornTop.imgUrl,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+const Item = (props) => {
+    key = props.index;
+
+    return (
+      <div className="itemBox">
+        <p><strong>&nbsp;&nbsp;{props.itemName}</strong></p>
+        <p>&nbsp;&nbsp;{props.itemColor}</p>
+        <p>&nbsp;&nbsp;{props.imgUrl}</p>{/* need to add img styling*/}
+        <div className="itemButton">
+          <input className="selectItemButton" onClick={() => {
+            console.log('tryon input received')}
+           } readOnly/>
+          <input className="deleteItemButton" onClick={() => {
+            console.log('delete input received')}
+           } readOnly/>
+        </div>
+      </div>
+    );};
+
+export default connect(mapStateToProps, mapDispatchToProps) (Item);
+
