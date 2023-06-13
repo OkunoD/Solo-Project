8fa9c609f3dc6857c09a82069fbea06c7949c43f
OkunoD <108437247+OkunoD@users.noreply.github.com>
2023-06-13 14:53:46 -0700

    fillWardrobe working, will fetch from db and set state to fill wardrobe components. parts breaking however

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,28 +1,29 @@
 // import actionType constants
 import * as types from '../constants/actionTypes';
 
-export const fetchWardrobeData = () => {
-  console.log('in fetchWardrobeData')
-  fetch('/api/items')
-    .then(console.log('after get fetch request'))
-    .then((response) => {
-      console.log('response is: ', response);
-      return response.json();
-    })
-    .then((data) => {
-      console.log('data is: ', data); // Display the retrieved items in the console
-      dispatchEvent({
-        type: 'FETCH_MONGO_DATA_SUCCESS',
-        payload: data,
-      })
-    })
-    .catch((error) => {
-      dispatchEvent({
-        type: 'FETCH_MONGO_DATA_ERROR',
-        payload: error.message,
-      });
-    });
-};
+export const fillWardrobeActionCreator = (data) => ({
+  type: types.FETCH_MONGO_DATA_SUCCESS,
+  payload: data,
+  // console.log('in fillWardrobeActionCreator')
+  // fetch('/api/items')
+  //   .then(() => {
+  //     console.log('after fillWardrobe fetch request')
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log('in fillWardrobe data is: ', data); // Display the retrieved items in the console
+  //     dispatch({
+  //       type: 'FETCH_MONGO_DATA_SUCCESS',
+  //       payload: data,
+  //     })
+  //   })
+  //   .catch((error) => {
+  //     dispatch({
+  //       type: 'FETCH_MONGO_DATA_ERROR',
+  //       payload: error.message,
+  //     });
+  //   });
+});
 
 export const addItemActionCreator = (itemType, name, imgUrl, color) => ({
   type: types.ADD_ITEM,
