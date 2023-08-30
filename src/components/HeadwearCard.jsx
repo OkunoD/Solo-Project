83cfa2556b50dba64e988580ff0e064de38f8adf
anonymous <anonymous@email.com>
2023-08-29 21:21:17 -0700

    imgs now render in clothing drawers

--- src/components/HeadwearCard.jsx
+++ src/components/HeadwearCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { deleteItemActionCreator, tryOnItemActionCreator } from '../actions/actions.js'
 import './styles.css'; 
@@ -10,7 +10,7 @@ const mapStateToProps = function(state, ownProps) {
     headwearId: state.headwearList[ownProps.index].id,
     headwearName: state.headwearList[ownProps.index].name,
     headwearColor: state.headwearList[ownProps.index].color,
-    imgUrl: state.headwearList[ownProps.index].imgUrl,
+    file: state.headwearList[ownProps.index].file,
     //state below comment not needed
     headwearList: state.headwearList,
     ownProps: ownProps,
@@ -25,22 +25,43 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const Headwear = (props) => {
-    key = props.index;
+  const [imageSrc, setImageSrc] = useState('');
 
-    return (
-      <div className="itemBox">
-        <p><strong>&nbsp;&nbsp;{props.headwearName}</strong></p>
-        <p>&nbsp;&nbsp;{props.headwearColor}</p>
-        <p>&nbsp;&nbsp;{props.imgUrl}</p>{/* need to add img styling*/}
-        <div className="itemButton">
-          <input className="selectItemButton" onClick={() => {
-            console.log('tryon input received');
-            props.tryOnItem('headwear', props.headwearId, 'Headwear')}} type="Submit" value="Try it on" readOnly/>
-          <input className="deleteItemButton" onClick={() => {
-            props.deleteItem('headwear', props.headwearId)}} type="Submit" value="Delete" readOnly/>
-        </div>
+  key = props.index;
+
+  console.log('props in headwear is: ', props)
+
+  const imageData = props.file ? props.file.data : null;
+  const contentType = props.contentType;
+
+  useEffect(() => {
+    // Convert ArrayBuffer to base64
+    const base64 = btoa(
+      new Uint8Array(imageData).reduce(
+        (data, byte) => data + String.fromCharCode(byte),
+        ''
+      )
+    );
+    setImageSrc(`data:${contentType};base64,${base64}`);
+  }, [props]);
+
+  return (
+    <div className="itemBox">
+      <div className="image-container">
+        <img src={imageSrc} alt="Retrieved from state" className="image-content" />
       </div>
-    );};
+      <p><strong>&nbsp;&nbsp;{props.headwearName}</strong></p>
+      <p>&nbsp;&nbsp;{props.headwearColor}</p>
+      <p>&nbsp;&nbsp;{props.imgUrl}</p>{/* need to add img styling*/}
+      <div className="itemButton">
+        <input className="selectItemButton" onClick={() => {
+          console.log('tryon input received');
+          props.tryOnItem('headwear', props.headwearId, 'Headwear')}} type="Submit" value="Try it on" readOnly/>
+        <input className="deleteItemButton" onClick={() => {
+          props.deleteItem('headwear', props.headwearId)}} type="Submit" value="Delete" readOnly/>
+      </div>
+    </div>
+  );};
 
 export default connect(mapStateToProps, mapDispatchToProps) (Headwear);
 
