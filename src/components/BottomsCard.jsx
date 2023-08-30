f4dd433bb9ca4896bcc9af7335f3bde0e4736d6b
anonymous <anonymous@email.com>
2023-08-29 22:14:34 -0700

    all drawers showing images for items. filled some mock data for testing as well

--- src/components/BottomsCard.jsx
+++ src/components/BottomsCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { deleteItemActionCreator, tryOnItemActionCreator } from '../actions/actions.js'
 import './styles.css'; 
@@ -10,7 +10,7 @@ const mapStateToProps = function(state, ownProps) {
     bottomId: state.bottomsList[ownProps.index].id,
     bottomName: state.bottomsList[ownProps.index].name,
     bottomColor: state.bottomsList[ownProps.index].color,
-    imgUrl: state.bottomsList[ownProps.index].imgUrl
+    file: state.bottomsList[ownProps.index].file
   };
 };
 
@@ -20,21 +20,54 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const Bottoms = (props) => {
-    key = props.index;
+  key = props.index;
+  const [imageSrc, setImageSrc] = useState('');
 
-    return (
-      <div className="itemBox">
-        <p><strong>&nbsp;&nbsp;{props.bottomName}</strong></p>
-        <p>&nbsp;&nbsp;{props.bottomColor}</p>
-        <p>&nbsp;&nbsp;{props.imgUrl}</p>{/* need to add img styling*/}
-        <div className="itemButton">
-        <input className="selectItemButton" onClick={() => {
-            console.log('tryon input received');
-            props.tryOnItem('bottoms', props.bottomId, 'Bottom')}} type="Submit" value="Try it on" readOnly/>
-          <input className="deleteItemButton" onClick={() => {
-            props.deleteItem('bottoms', props.bottomId)}} type="Submit" value="Delete" readOnly/>
-        </div>
+  const imageData = props.file ? props.file.data : null;
+  const contentType = props.contentType;
+  
+
+  useEffect(() => {
+    // Convert ArrayBuffer to base64
+    const base64 = btoa(
+      new Uint8Array(imageData).reduce(
+        (data, byte) => data + String.fromCharCode(byte),
+        ''
+      )
+    );
+    setImageSrc(`data:${contentType};base64,${base64}`);
+  }, [props]);
+
+  const handleDelete = (itemId) => {
+    fetch(`/api/items/${itemId}`, {
+      method: "DELETE",
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.error('Error deleting item:', error);
+    });
+  };
+
+  return (
+    <div className="itemBox">
+      <div className="image-container">
+        <img src={imageSrc} alt="Retrieved from state" className="image-content" />
       </div>
-    );};
+      <p><strong>&nbsp;&nbsp;{props.bottomName}</strong></p>
+      <p>&nbsp;&nbsp;{props.bottomColor}</p>
+      <p>&nbsp;&nbsp;{props.imgUrl}</p>{/* need to add img styling*/}
+      <div className="itemButton">
+      <input className="selectItemButton" onClick={() => {
+          console.log('tryon input received');
+          props.tryOnItem('bottoms', props.bottomId, 'Bottom')}} type="Submit" value="Try it on" readOnly/>
+        <input className="deleteItemButton" onClick={() => {
+          handleDelete(props.bottomId);
+          props.deleteItem('bottoms', props.bottomId)}} type="Submit" value="Delete" readOnly/>
+      </div>
+    </div>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps) (Bottoms);
--- src/components/ShoesCard.jsx
+++ src/components/ShoesCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { deleteItemActionCreator, tryOnItemActionCreator } from '../actions/actions.js'
 import './styles.css'; 
@@ -10,7 +10,7 @@ const mapStateToProps = function(state, ownProps) {
     shoesId: state.shoesList[ownProps.index].id,
     shoesName: state.shoesList[ownProps.index].name,
     shoesColor: state.shoesList[ownProps.index].color,
-    imgUrl: state.shoesList[ownProps.index].imgUrl
+    file: state.shoesList[ownProps.index].file
   };
 };
 
@@ -21,20 +21,53 @@ const mapDispatchToProps = (dispatch) => ({
 
 const Shoes = (props) => {
     key = props.index;
+    const [imageSrc, setImageSrc] = useState('');
 
-    return (
-      <div className="itemBox">
-        <p><strong>&nbsp;&nbsp;{props.shoesName}</strong></p>
-        <p>&nbsp;&nbsp;{props.shoesColor}</p>
-        <p>&nbsp;&nbsp;{props.imgUrl}</p>{/* need to add img styling*/}
-        <div className="itemButton">
-        <input className="selectItemButton" onClick={() => {
-            console.log('tryon input received');
-            props.tryOnItem('shoes', props.shoesId, 'Shoes')}} type="Submit" value="Try it on" readOnly/>
-          <input className="deleteItemButton" onClick={() => {
-            props.deleteItem('shoes', props.shoesId)}} type="Submit" value="Delete" readOnly/>
-        </div>
+  const imageData = props.file ? props.file.data : null;
+  const contentType = props.contentType;
+  
+
+  useEffect(() => {
+    // Convert ArrayBuffer to base64
+    const base64 = btoa(
+      new Uint8Array(imageData).reduce(
+        (data, byte) => data + String.fromCharCode(byte),
+        ''
+      )
+    );
+    setImageSrc(`data:${contentType};base64,${base64}`);
+  }, [props]);
+
+  const handleDelete = (itemId) => {
+    fetch(`/api/items/${itemId}`, {
+      method: "DELETE",
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.error('Error deleting item:', error);
+    });
+  };
+
+  return (
+    <div className="itemBox">
+      <div className="image-container">
+        <img src={imageSrc} alt="Retrieved from state" className="image-content" />
       </div>
-    );};
+      <p><strong>&nbsp;&nbsp;{props.shoesName}</strong></p>
+      <p>&nbsp;&nbsp;{props.shoesColor}</p>
+      <p>&nbsp;&nbsp;{props.imgUrl}</p>{/* need to add img styling*/}
+      <div className="itemButton">
+      <input className="selectItemButton" onClick={() => {
+          console.log('tryon input received');
+          props.tryOnItem('shoes', props.shoesId, 'Shoes')}} type="Submit" value="Try it on" readOnly/>
+        <input className="deleteItemButton" onClick={() => {
+          handleDelete(props.shoesId);
+          props.deleteItem('shoes', props.shoesId)}} type="Submit" value="Delete" readOnly/>
+      </div>
+    </div>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps) (Shoes);
