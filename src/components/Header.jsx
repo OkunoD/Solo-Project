9ea30159745bf0c003c8492333f67264ea6f9b96
OkunoD <okunoderek@gmail.com>
2023-09-06 22:39:07 -0700

    auth login with google seems to be working, as well as log out. need to add functionality next

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,13 @@ import React, {useContext} from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <p>HEADER HERE</p>
+        <div className="">
+            <header>HEADER HERE
+                <p>hello</p>
+                <a href="/auth/google" className="btn">Login or Sign-Up Here</a>
+                <a href="/logout" className="btn">Log Out</a>
+            </header>
+            
         </div>
     )
 }
