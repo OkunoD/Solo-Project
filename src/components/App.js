27b3adcfda015762aa0489cb9e5f7183b356e9b7
OkunoD <okunoderek@gmail.com>
2023-09-08 16:42:01 -0700

    added route for saved outfits, added skeleton for adding outfit functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header.jsx';
 import WardrobeContainer from '../containers/WardrobeContainer.jsx'
+import OutfitContainer from '../containers/OutfitContainer.jsx';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
@@ -12,6 +13,7 @@ const App = () => {
             <Header/>
             <Routes>
                 <Route exact path="/" element={<WardrobeContainer/>}/>
+                <Route exact path="/outfits" element={<OutfitContainer/>}/>
             </Routes>
         </Router>
     );
