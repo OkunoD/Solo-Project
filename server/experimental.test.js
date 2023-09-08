4d2e3117c584761a5ed8dbd384f35d338116847e
anonymous <anonymous@email.com>
2023-09-08 14:36:03 -0700

    installed jest testing

---
+++ server/experimental.test.js
@@ -0,0 +1,9 @@
+import request from 'supertest';
+import express from 'express';
+import router from '../routes/main.js';
+
+describe("experimental test", () => {
+    test("my first test", () => {
+        expect(1).toEqual(1);
+    })
+})
