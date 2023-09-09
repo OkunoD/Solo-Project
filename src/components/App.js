4d4afe46ec4099728838815746d7ebbeec0caec8
anonymous <anonymous@email.com>
2023-09-08 18:08:14 -0700

    added more features to outfit container and moved wardrobefill higher to app component so it persists when changing from closet to outfits

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useSelector, connect } from 'react-redux';
 import Header from './Header.jsx';
+import { fillWardrobeActionCreator } from '../actions/actions';
 import WardrobeContainer from '../containers/WardrobeContainer.jsx'
 import OutfitContainer from '../containers/OutfitContainer.jsx';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
@@ -7,7 +9,34 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 // import './styles.css';
 
-const App = () => {
+const mapDispatchToProps = dispatch => ({
+    fillWardrobe : (payload) => dispatch(fillWardrobeActionCreator(payload)),
+});
+
+const App = (props) => {
+    const state = useSelector((state) => state);
+
+    useEffect(() => {
+        console.log('useeffect hit')
+        if (process.env.NODE_ENV==='development') {
+          console.log({mockData});
+          props.fillWardrobe(mockData);
+        } else {
+          fetch('/api/items')
+            .then((response) => {
+              console.log('response is: ', response);
+              return response.json();
+            })
+            .then((data) => {
+              console.log('data is: ', data);
+              props.fillWardrobe(data);
+            })
+            .catch((error) => {
+              console.error('Error:', error);
+            });
+        }
+      }, []);
+
     return(
         <Router>
             <Header/>
@@ -19,4 +48,4 @@ const App = () => {
     );
 }
 
-export default App;
+export default connect(null, mapDispatchToProps) (App);
