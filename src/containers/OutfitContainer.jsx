efe9a08cbe60fff63ca1737f787c20a9a3e278c1
OkunoD <okunoderek@gmail.com>
2023-09-08 17:48:45 -0700

    basic outfit component made, can retrieve outfits from db now and set to state

--- src/containers/OutfitContainer.jsx
+++ src/containers/OutfitContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 // import WardrobeReducer from '../components/WardrobeReducer.js';
@@ -9,13 +9,42 @@ import Dispatch from 'react';
 const mapStateToProps = state => ({  
 });
   
-function OutfitContainer() {
+const OutfitContainer = () => {
+
+    //fetch outfits from db (outfits have {name: string, outfit: array of ids})
+    //use outfit array to fetch entire items from state to then fill an outfit card
+
+    const [outfits, setOutfits] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/outfits')
+        .then((response) => {
+            console.log('outfits fetch response is: ', response);
+            return response.json();
+        })
+        .then((data) => {
+            console.log('data is: ', data);
+            setOutfits(data);
+        }).catch((error) => {
+            console.log('Error fetching outfits:', error);
+        });
+    }, [outfits]);
+
+
   return(
     <div className="outfit-container">
       <div className="outerBox">
         <div className="header"><strong>&nbsp;&nbsp;MY OUTFITS</strong></div>
-
-      </div>
+        <div>
+            {outfits.map((outfit, index) => (
+                <div key={index} className="outfit-text">
+                    <strong>Outfit Name:</strong> {outfit.name}
+                    <br />
+                    <strong>Outfit Items:</strong> {outfit.outfit.join(', ')}
+                </div>
+            ))}
+            </div>
+        </div>
     </div>
   );
 }
