01ff973f28c5e9c87608c2b0127840bec87d22da
OkunoD <okunoderek@gmail.com>
2023-09-15 17:07:51 -0700

    applied re-rendering after adds for all categories, also applied brand and sizing rendering on all components

--- src/components/JacketsCard.jsx
+++ src/components/JacketsCard.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { deleteItemActionCreator, tryOnItemActionCreator } from '../actions/actions.js'
-
+import { deleteItemActionCreator, tryOnItemActionCreator, openAlert, closeAlert } from '../actions/actions.js'
 
 let key = undefined;
 
@@ -17,6 +16,8 @@ const mapStateToProps = function(state, ownProps) {
 };
 
 const mapDispatchToProps = (dispatch) => ({
+  openAlert : (payload)  => dispatch(openAlert(payload)),
+  closeAlert : ()  => dispatch(closeAlert()),
   tryOnItem : (payload1, payload2, payload3) => dispatch(tryOnItemActionCreator(payload1, payload2, payload3)),
   deleteItem : (payload1, payload2) => dispatch(deleteItemActionCreator(payload1, payload2)),
 });
@@ -30,26 +31,56 @@ const Jackets = (props) => {
     const contentType = props.contentType;
 
     useEffect(() => {
-      // Convert ArrayBuffer to base64
       const base64 = btoa(
         new Uint8Array(imageData).reduce(
           (data, byte) => data + String.fromCharCode(byte),
           ''
-        )
-      );
-      setImageSrc(`data:${contentType};base64,${base64}`);
-    }, [props]);
-  
-    const handleDelete = (itemId) => {
-      fetch(`/api/items/${itemId}`, {
-        method: "DELETE",
-      })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
+          )
+          );
+          setImageSrc(`data:${contentType};base64,${base64}`);
+          //  if (!imageSrc) codeblock below is to render newly added items without refresh. 
+          //  probably could use reworking, but works.  
+          if (!imageSrc) {
+            setTimeout(()=> {
+              fetch(`api/items/${props.id}`)
+              .then((response) => {
+                return response.json();
+              }).then((item) => {
+                const altBase64 = btoa(
+                  new Uint8Array(item[0].file.data).reduce(
+                    (data, byte) => data + String.fromCharCode(byte),
+                    ''
+                    )
+                    );
+                    setImageSrc(`data:${item[0].contentType};base64,${altBase64}`);
+                  })
+                  .catch((error)=>{
+                    console.error('Error fetching item', error.message);
+                  });
+                }, 500);
+              } 
+            }, []);
+    
+    const toggleAlert = (message) => {
+      console.log('inside toggleAlert, message is', message);
+      props.openAlert(message);
+    }
+
+    const handleDelete = async (itemId) => {
+      try {
+        const response = await fetch(`/api/items/${itemId}`, {
+          method: "DELETE",
+        });
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log('inside handleDelete, data.message is: ',data.message);
+          toggleAlert(data.message);
+        } else {
+          throw new Error('Error deleting item');
+        }
+      } catch(error) {
         console.error('Error deleting item:', error);
-      });
+      }
     };
 
     return (
@@ -62,12 +93,10 @@ const Jackets = (props) => {
             <div className="designer-and-size">
             <p className="item-brand">{props.brand ? props.brand : 'no brand'}</p>
             <p className="item-size">{props.size ? props.size: 'no size'}</p>
-          </div>
+            </div>
           <p className="item-name">{props.name}</p>
           <div className="item-button-div">
             <input className="black-button" onClick={() => {
-              console.log('tryon input received');
-              console.log('jacketId is: ', props.id);
               props.tryOnItem('jackets', props.id, 'Jacket')}} type="Submit" value="Try it on" readOnly/>
             <input className="red-button" onClick={() => {
               handleDelete(props.id);
