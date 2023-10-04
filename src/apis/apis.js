a22f00d9c06a55d7874349140377970b7d416173
OkunoD <okunoderek@gmail.com>
2023-10-04 01:55:40 -0700

    added search api to pull cities with longitude/latitude

---
+++ src/apis/apis.js
@@ -0,0 +1,10 @@
+
+
+export const geoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '092ed1fc4amshaeffe4a8d904b76p1772b4jsnb40e7fc4e62f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
--- src/components/WeatherModal.jsx
+++ src/components/WeatherModal.jsx
@@ -2,6 +2,7 @@ import React, { Component, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import JacketsCard from './JacketsCard.jsx';
 import ItemCard from './ItemCard.jsx';
+import { AsyncPaginate } from 'react-select-async-paginate';
 import { 
     openAlert, 
     closeAlert, 
@@ -9,6 +10,47 @@ import {
     clearOutfitActionCreator,
     lockItemActionCreator
  } from '../actions/actions.js'
+ import { geoApiOptions, geoApiUrl } from '../apis/apis.js';
+
+
+ export const Search = ({onSearchChange}) => {
+
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = async (inputValue) => {
+        try {
+            const response = await fetch(`${geoApiUrl}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions);
+            const result = await response.json();
+            console.log(result);
+            return {
+                options: result.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.region}, ${city.countryCode}`,
+                    }
+                })
+            }
+        } catch (error) { 
+            console.error(error);
+        }
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate
+            placeholder="Search city"
+            debounceTimeout={500}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    )
+    
+ } 
 
  export const WeatherModal = (props) => {
     const [data, setData] = useState(props.data);
@@ -22,8 +64,16 @@ import {
     console.log("props.data", props.data);
     console.log("data", data);
 
+    const handleOnSearchChange = (searchData) => {
+        console.log(searchData);
+    }
+
+
     return (
         <div className="weather-modal">
+            <Search 
+                onSearchChange={handleOnSearchChange}
+            />
             <h1>weather!</h1>
             <p>{data.weather[0].id}</p>
         </div>
